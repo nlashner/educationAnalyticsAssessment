aaaaf351c5e8c7eb0da861ae3debab4dd5c0dd3d
Nora Lashner <nlashner@gmail.com>
2020-06-23 15:30:07 -0400

    tested out program percentages donut chart with canvas.js

--- assessment/src/App.js
+++ assessment/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home/Home'
+import ProgramPercentages from './components/ProgramPercentages/ProgramPercentages'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <ProgramPercentages />
     </div>
   );
 }
--- assessment/src/components/Home/Home.js
+++ assessment/src/components/Home/Home.js
@@ -1,40 +1,38 @@
 import React, {Component} from 'react'
 import axios from 'axios'
+import './home.css'
 
 class Home extends Component {
   constructor(){
     super()
     this.state = {
-      schoolName: '',
-      schoolWebsite: '',
-      schoolCity: '',
-      schoolState: '',
-      schoolZip: '',
-      numUndergradute: 0,
-      numGraduate: 0,
+      schoolName: 'University of Wisconsin-Madison',
+      schoolWebsite: 'www.wisc.edu',
+      schoolCity: 'Madison',
+      schoolState: 'WI',
+      schoolZip: '53706',
+      numUndergradute: 32962,
+      numGraduate: 13058,
       programPercentageObj: {},
       raceEthnicity: {}
     }
   }
 
   async componentDidMount(){
-    let queryString = 'https://api.data.gov/ed/collegescorecard/v1/schools/?school.operating=1&2015.academics.program_available.assoc_or_bachelors=true&2015.student.size__range=1..&school.degrees_awarded.predominant__range=1..3&school.degrees_awarded.highest__range=2..4&id=240444&api_key='
-    let apiKey = 'wx5ciB6nDQx9zNRBxYgsr44yww3Jp5IZgP6tLMZC'
-    let { data } = await axios.get(queryString + apiKey)
-
-   //console.log(data.results[0]['2012'].student.enrollment.undergrad_12_month)
-
-    this.setState({
-      schoolName: data.results[0].school.name,
-      schoolWebsite: data.results[0].school.school_url,
-      schoolCity: data.results[0].school.city,
-      schoolState: data.results[0].school.state,
-      schoolZip: data.results[0].school.zip.slice(0,5),
-      numUndergraduate: data.results[0]['2012'].student.enrollment.undergrad_12_month,
-      numGraduate: data.results[0]['2012'].student.enrollment.grad_12_month,
-      programPercentageObj: data.results[0]['2012'].academics.program_percentage,
-      raceEthnicity: data.results[0]['2012'].student.demographics.race_ethnicity
-    })
+    // let queryString = 'https://api.data.gov/ed/collegescorecard/v1/schools/?school.operating=1&2015.academics.program_available.assoc_or_bachelors=true&2015.student.size__range=1..&school.degrees_awarded.predominant__range=1..3&school.degrees_awarded.highest__range=2..4&id=240444&api_key='
+    // let apiKey = 'wx5ciB6nDQx9zNRBxYgsr44yww3Jp5IZgP6tLMZC'
+    // let { data } = await axios.get(queryString + apiKey)
+    // this.setState({
+    //   schoolName: data.results[0].school.name,
+    //   schoolWebsite: data.results[0].school.school_url,
+    //   schoolCity: data.results[0].school.city,
+    //   schoolState: data.results[0].school.state,
+    //   schoolZip: data.results[0].school.zip.slice(0,5),
+    //   numUndergraduate: data.results[0]['2012'].student.enrollment.undergrad_12_month,
+    //   numGraduate: data.results[0]['2012'].student.enrollment.grad_12_month,
+    //   programPercentageObj: data.results[0]['2012'].academics.program_percentage,
+    //   raceEthnicity: data.results[0]['2012'].student.demographics.race_ethnicity
+    // })
 
   }
 
@@ -43,14 +41,21 @@ class Home extends Component {
 
     if(!this.state.schoolName.length) return <h1>loading...</h1>
 
-    console.log(this.state)
     return (
       <div>
-        <div>{this.state.schoolName}</div>
+        <div className='schoolName'>{this.state.schoolName}</div>
+        <div className='school-info-container'>
         <div>{this.state.schoolWebsite}</div>
         <div>{this.state.schoolCity}, {this.state.schoolState}, {this.state.schoolZip}</div>
-        <div>Total Students: {this.state.numUndergraduate + this.state.numGraduate}</div>
-        <div>Total Undergrad: {this.state.numUndergraduate}, Total Graduate{this.state.numGraduate}</div>
+        </div>
+
+        <div className='students-container'>
+        <div>Total Students: {this.state.numUndergradute + this.state.numGraduate}</div>
+        <div className='student-info'>
+        <div>Total Undergrad: {this.state.numUndergradute}</div>
+        <div>Total Graduate{this.state.numGraduate}</div>
+        </div>
+        </div>
       </div>
     )
   }
