a22753914ad3aa0e9c95d6b584d2ac88556755e0
Nora Lashner <nlashner@gmail.com>
2020-06-23 11:47:33 -0400

    created and formatted the Navbar

--- assessment/src/components/Navbar/Navbar.js
+++ assessment/src/components/Navbar/Navbar.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import logo from '../../eAlogo.jpg'
+import './navbar.css'
 
 const Navbar = () => {
   return (
-    <div>
-
+    <div className='nav-container'>
+      <img src={logo} alt='education-analytics-logo' className='nav-logo'></img>
+      <div>Nora Lashner</div>
     </div>
   )
 }
+
+export default Navbar
