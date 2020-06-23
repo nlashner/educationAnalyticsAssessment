ec03ef655ed4278e089b6d675e9a76d6114b638f
Nora Lashner <nlashner@gmail.com>
2020-06-23 12:39:16 -0400

    all necesary data is pull from API into the state

--- assessment/src/components/Home/Home.js
+++ assessment/src/components/Home/Home.js
@@ -10,7 +10,10 @@ class Home extends Component {
       schoolCity: '',
       schoolState: '',
       schoolZip: '',
-      totalNumStudents: 0,
+      numUndergradute: 0,
+      numGraduate: 0,
+      programPercentageObj: {},
+      raceEthnicity: {}
     }
   }
 
@@ -18,13 +21,21 @@ class Home extends Component {
     let queryString = 'https://api.data.gov/ed/collegescorecard/v1/schools/?school.operating=1&2015.academics.program_available.assoc_or_bachelors=true&2015.student.size__range=1..&school.degrees_awarded.predominant__range=1..3&school.degrees_awarded.highest__range=2..4&id=240444&api_key='
     let apiKey = 'wx5ciB6nDQx9zNRBxYgsr44yww3Jp5IZgP6tLMZC'
     let { data } = await axios.get(queryString + apiKey)
+
+   //console.log(data.results[0]['2012'].student.enrollment.undergrad_12_month)
+
     this.setState({
       schoolName: data.results[0].school.name,
       schoolWebsite: data.results[0].school.school_url,
       schoolCity: data.results[0].school.city,
       schoolState: data.results[0].school.state,
       schoolZip: data.results[0].school.zip.slice(0,5),
+      numUndergraduate: data.results[0]['2012'].student.enrollment.undergrad_12_month,
+      numGraduate: data.results[0]['2012'].student.enrollment.grad_12_month,
+      programPercentageObj: data.results[0]['2012'].academics.program_percentage,
+      raceEthnicity: data.results[0]['2012'].student.demographics.race_ethnicity
     })
+
   }
 
 
@@ -33,12 +44,13 @@ class Home extends Component {
     if(!this.state.schoolName.length) return <h1>loading...</h1>
 
     console.log(this.state)
-
     return (
       <div>
         <div>{this.state.schoolName}</div>
         <div>{this.state.schoolWebsite}</div>
         <div>{this.state.schoolCity}, {this.state.schoolState}, {this.state.schoolZip}</div>
+        <div>Total Students: {this.state.numUndergraduate + this.state.numGraduate}</div>
+        <div>Total Undergrad: {this.state.numUndergraduate}, Total Graduate{this.state.numGraduate}</div>
       </div>
     )
   }
