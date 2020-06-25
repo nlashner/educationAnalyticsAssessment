bc6db4ea93869181b0fbbcbfd8b770f3822a55b0
Nora Lashner <nlashner@gmail.com>
2020-06-25 15:47:20 -0400

    updated design on Navbar

--- assessment/src/components/Navbar/Navbar.js
+++ assessment/src/components/Navbar/Navbar.js
@@ -1,17 +1,20 @@
 import React from 'react'
-import logo from '../../eAlogo.jpg'
-import Print from '../Print/Print'
-import { AiOutlineFilePdf } from 'react-icons/ai'
+import logo from '../../EAlogo.png'
+import { AiOutlineFilePdf, AiFillPrinter } from 'react-icons/ai'
 import './navbar.css'
 
 const Navbar = (props) => {
   const { savePDF } = props
   return (
     <div className='nav-container'>
+      <div className='nav-left'>
       <img src={logo} alt='education-analytics-logo' className='nav-logo'></img>
-      <div>Nora Lashner</div>
-      <AiOutlineFilePdf onClick={savePDF}/>
-      <Print />
+      <div className='nav-name'>Nora Lashner</div>
+      </div>
+      <div className='nav-right'>
+      <AiOutlineFilePdf className='nav-icon' onClick={savePDF}/>
+      <AiFillPrinter className='nav-icon' onClick={() => window.print()}/>
+      </div>
     </div>
   )
 }
